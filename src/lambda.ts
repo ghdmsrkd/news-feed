94270d0c1b6e8016a6281aa95dcab7d5ed554a91
devhipo <ghdmsrkd@naver.com>
2022-06-14 22:29:22 +0900

    init : nest + serverless 기본 세팅

---
+++ src/lambda.ts
@@ -0,0 +1,41 @@
+import { Handler, Context } from "aws-lambda"
+import { Server } from "http"
+import { createServer, proxy } from "aws-serverless-express"
+import { eventContext } from "aws-serverless-express/middleware"
+
+import { NestFactory } from "@nestjs/core"
+import { ExpressAdapter } from "@nestjs/platform-express"
+import { AppModule } from "./app.module"
+
+import express = require("express")
+import { setupSwagger } from "./common/nest/swagger"
+
+const binaryMimeTypes: string[] = []
+
+let cachedServer: Server
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express()
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    )
+    nestApp.use(eventContext())
+    setupSwagger(nestApp)
+    await nestApp.init()
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes)
+  }
+  return cachedServer
+}
+
+export const handler: Handler = async (event: any, context: Context) => {
+  if (event.path === "/api") {
+    event.path = "/api/"
+  }
+  event.path = event.path.includes("swagger-ui")
+    ? `/api${event.path}`
+    : event.path
+  cachedServer = await bootstrapServer()
+  return proxy(cachedServer, event, context, "PROMISE").promise
+}
