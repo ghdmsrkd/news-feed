8663d34a1dd570d69725839f8543683dbd564a41
hama <hama@vonvon.me>
2022-06-16 12:31:07 +0900

    feat : SchoolNewsRepository 모듈 연결

--- src/api/admin/school/admin-school.module.ts
+++ src/api/admin/school/admin-school.module.ts
@@ -3,10 +3,16 @@ import SchoolRepository from "../../../common/database/ddb/school/school.repo"
 import AdminRepository from "../../../common/database/ddb/admin/admin.repo"
 import { AdminSchoolController } from "./admin-school.controller"
 import { AdminSchoolService } from "./admin-school.service"
+import SchoolNewsRepository from "../../../common/database/ddb/school-news/school-news.repo"
 
 @Module({
   imports: [],
   controllers: [AdminSchoolController],
-  providers: [AdminSchoolService, AdminRepository, SchoolRepository],
+  providers: [
+    AdminSchoolService,
+    AdminRepository,
+    SchoolRepository,
+    SchoolNewsRepository,
+  ],
 })
 export class AdminSchoolModule {}
--- src/common/database/ddb/ddb.module.ts
+++ src/common/database/ddb/ddb.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from "@nestjs/common"
 import AdminRepository from "./admin/admin.repo"
+import SchoolNewsRepository from "./school-news/school-news.repo"
 import SchoolRepository from "./school/school.repo"
 
 @Global()
 @Module({
-  providers: [AdminRepository, SchoolRepository],
-  exports: [AdminRepository, SchoolRepository],
+  providers: [AdminRepository, SchoolRepository, SchoolNewsRepository],
+  exports: [AdminRepository, SchoolRepository, SchoolNewsRepository],
 })
 export class GlobalDDBModule {}
