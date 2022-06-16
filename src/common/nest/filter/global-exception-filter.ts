8cce1365a7a6af6140229e103738c54b86f14bb2
hama <hama@vonvon.me>
2022-06-16 17:58:27 +0900

    feat : 글로벌 예외 처리 추가

---
+++ src/common/nest/filter/global-exception-filter.ts
@@ -0,0 +1,49 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common"
+
+import { Request } from "express"
+
+@Catch()
+export class GlobalExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse()
+    const request: Request = ctx.getRequest()
+    const error: HttpException | Error =
+      exception instanceof HttpException ? exception : <Error>exception
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR
+
+    try {
+      response.status(status).json({
+        result: false,
+        code: status,
+        message:
+          error instanceof HttpException &&
+          typeof error.getResponse() === "object"
+            ? (<{ [key: string]: any }>error.getResponse()).message
+            : error.message,
+        timestamp: new Date().toISOString(),
+        method: request.method,
+        path: request.url,
+      })
+    } catch (err) {
+      response.status(500).json({
+        result: false,
+        code: 500,
+        message: "something wrong when server is running",
+        timestamp: new Date().toISOString(),
+        method: request.method,
+        path: request.url,
+      })
+    }
+  }
+}
