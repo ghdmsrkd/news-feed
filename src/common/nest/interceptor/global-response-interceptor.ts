9b7d7e377048dcffe63416f76684896f679a2373
hama <hama@vonvon.me>
2022-06-16 17:58:42 +0900

    feat : 글로벌 응답 인터셉처 추가

---
+++ src/common/nest/interceptor/global-response-interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpStatus,
+} from "@nestjs/common"
+
+import { Observable } from "rxjs"
+import { map } from "rxjs/operators"
+
+@Injectable()
+export class GlobalResponseInterceptor implements NestInterceptor {
+  intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
+    const date = new Date()
+
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          status: HttpStatus.ACCEPTED,
+          timestamp: date.toISOString(),
+          result: data,
+        }
+      }),
+    )
+  }
+}
