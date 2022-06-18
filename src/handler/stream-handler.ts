88f5a904ecdf50200ce2b46dcf76f2fecb7497e7
hama <hama@vonvon.me>
2022-06-19 02:02:57 +0900

    feat : 이미지 컨버터 추가, 분기 로직 수정

--- src/handler/stream-handler.ts
+++ src/handler/stream-handler.ts
@@ -2,6 +2,8 @@ import { NestFactory } from "@nestjs/core"
 import { StreamModule } from "../stream/stream.module"
 import { TaskService } from "../stream/task/task.service"
 import { mockEvent } from "../stream/mock-event"
+import * as AWS from "aws-sdk"
+import { ISchoolNews } from "../common/database/ddb/school-news/school-news.model"
 
 exports.streamHandler = async (event) => {
   if (process.env.NODE_ENV === "dev") {
@@ -11,14 +13,28 @@ exports.streamHandler = async (event) => {
   const app = await NestFactory.createApplicationContext(StreamModule)
   const tasksService = app.get(TaskService)
 
-  for (const recod of event.Records) {
-    console.log(recod)
-    if (recod.eventName === "INSERT") {
-      const insertResult = await tasksService.insertFeedEachStudents("student2")
-    } else if (recod.eventName === "UPDATE") {
-      const updateResult = await tasksService.updateFeedEachStudents()
+  for (const record of event.Records) {
+    console.log(record)
+
+    const schoolNews = convertNewImageToSchoolNews(
+      record.dynamodb.NewImage,
+    ) as ISchoolNews
+
+    if (record.eventName === "INSERT") {
+      // 새로운 학교 뉴스가 생성 되었을 때
+      const insertResult = await tasksService.insertFeedEachStudents(schoolNews)
+    } else if (record.eventName === "MODIFY") {
+      // 기존 학교 뉴스가 수정 되어 을 때
+      const updateResult = await tasksService.updateFeedEachStudents(schoolNews)
     } else {
-      return null
+      // 학교 뉴스가 삭제 되었을 때
+      continue
     }
   }
+}
+
+const convertNewImageToSchoolNews = (newImage: any) => {
+  const unmarshalled = AWS.DynamoDB.Converter.unmarshall(newImage)
+  console.log(unmarshalled)
+  return unmarshalled
 }
