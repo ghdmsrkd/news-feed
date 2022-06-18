85e212e7a522574d8424bdf76961174a4dc490da
hama <hama@vonvon.me>
2022-06-19 05:36:07 +0900

    feat : 학교 생서 시 중복 확인 로직 추가

--- src/common/database/ddb/school/school.repo.ts
+++ src/common/database/ddb/school/school.repo.ts
@@ -2,7 +2,8 @@ import * as dynamoose from "dynamoose"
 import { SchoolModel } from "./school.model"
 import { Model } from "dynamoose/dist/Model"
 import { SchoolSchema } from "./school.schema"
-import { Injectable } from "@nestjs/common"
+import { HttpStatus, Injectable } from "@nestjs/common"
+import { NestError } from "../../../../common/nest/exception/nest-error"
 
 @Injectable()
 export default class SchoolRepository {
@@ -14,8 +15,10 @@ export default class SchoolRepository {
   }
 
   async createOneSchool(admin_id: string, location: string, name: string) {
+    const school_code = `${location}-${name}`
+    await this.checkItem(school_code)
     return await this.dbInstance.create({
-      school_code: `${location}-${name}`,
+      school_code: school_code,
       admin_id: admin_id,
       location: location,
       name: name,
@@ -24,6 +27,12 @@ export default class SchoolRepository {
 
   async getSchoolByCode(code: string) {
     return await this.dbInstance.get({ school_code: code })
+  }
+
+  async checkItem(id: string) {
+    if (await this.getSchoolByCode(id)) {
+      throw new NestError(HttpStatus.FORBIDDEN, "해당 항목이 이미 존제 합니다.")
+    }
   }
 
   async querySchoolByAdminId(admin_id: string) {
