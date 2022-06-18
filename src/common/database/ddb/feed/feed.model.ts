784b729970aca050462afc4d7ee3249cac1b4ac4
hama <hama@vonvon.me>
2022-06-19 01:53:55 +0900

    feat : 모델 수정

--- src/common/database/ddb/feed/feed.model.ts
+++ src/common/database/ddb/feed/feed.model.ts
@@ -1,6 +1,8 @@
 import { Document } from "dynamoose/dist/Document"
 
 export class FeedNewsModel extends Document {
+  feed_id = ""
+  student_id = ""
   school_news_id = ""
   created_at = 0
   school_code = ""
