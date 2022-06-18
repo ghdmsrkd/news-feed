e4bcc25dfb4127b336beb13d96d6ede614b6c635
hama <hama@vonvon.me>
2022-06-19 04:02:08 +0900

    comment

--- src/common/nest/guard/admin.guard.ts
+++ src/common/nest/guard/admin.guard.ts
@@ -14,7 +14,7 @@ export class AdminGuard implements CanActivate {
   async canActivate(ctx: ExecutionContext): Promise<boolean> {
     const request = ctx.switchToHttp().getRequest()
 
-    // jwt 토큰을 발급 하진 않았지만 admin_id를 jwt 토큰이라고 가정하고 인증 과정을 거친다.
+    // jwt 토큰을 발급 하진 않았지만 admin_id를 jwt 토큰이라고 가정하고 인증/인가 과정을 거친다.
     const token: string = request.headers.authorization?.split("Bearer ")[1]
     const adminModel = await this.adminRepository.getAdminById(token)
     request.payload = {
--- src/common/nest/guard/student.guard.ts
+++ src/common/nest/guard/student.guard.ts
@@ -15,7 +15,7 @@ export class StudentGuard implements CanActivate {
   async canActivate(ctx: ExecutionContext): Promise<boolean> {
     const request = ctx.switchToHttp().getRequest()
 
-    // jwt 토큰을 발급 하진 않았지만 student_id를 jwt 토큰이라고 가정하고 인증 과정을 거친다.
+    // jwt 토큰을 발급 하진 않았지만 student_id를 jwt 토큰이라고 가정하고 인증/인가 과정을 거친다.
     const token: string = request.headers.authorization?.split("Bearer ")[1]
     const studentModel = await this.studentRepository.getStudentById(token)
     request.payload = {
