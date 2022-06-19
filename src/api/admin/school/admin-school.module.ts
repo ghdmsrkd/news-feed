88d407538158790689b652cf99edb7e9d29f4720
hama <hama@vonvon.me>
2022-06-19 14:41:43 +0900

    fix : 불필요하게 인젝된 레포 삭제

--- src/api/admin/school/admin-school.module.ts
+++ src/api/admin/school/admin-school.module.ts
@@ -1,18 +1,10 @@
 import { Module } from "@nestjs/common"
-import SchoolRepository from "../../../common/database/ddb/school/school.repo"
-import AdminRepository from "../../../common/database/ddb/admin/admin.repo"
 import { AdminSchoolController } from "./admin-school.controller"
 import { AdminSchoolService } from "./admin-school.service"
-import SchoolNewsRepository from "../../../common/database/ddb/school-news/school-news.repo"
 
 @Module({
   imports: [],
   controllers: [AdminSchoolController],
-  providers: [
-    AdminSchoolService,
-    AdminRepository,
-    SchoolRepository,
-    SchoolNewsRepository,
-  ],
+  providers: [AdminSchoolService],
 })
 export class AdminSchoolModule {}
--- src/api/student/school/school.module.ts
+++ src/api/student/school/school.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common"
-import SubscribeRepository from "../../../common/database/ddb/subscribe/subscribe.repo"
-import StudentRepository from "../../../common/database/ddb/student/student.repo"
 import { SchoolController } from "./school.controller"
 import { SchoolService } from "./school.service"
 
 @Module({
   imports: [],
   controllers: [SchoolController],
-  providers: [SchoolService, StudentRepository, SubscribeRepository],
+  providers: [SchoolService],
 })
 export class SchoolModule {}
