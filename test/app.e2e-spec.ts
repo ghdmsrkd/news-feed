d19acaa3e0bcf2643ffe919c6bf7b51bea914632
hama <hama@vonvon.me>
2022-06-19 16:07:08 +0900

    feat : e2e 테스트 셋업

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,48 @@
 import { Test, TestingModule } from "@nestjs/testing"
-import { INestApplication } from "@nestjs/common"
+import { INestApplication, ValidationPipe } from "@nestjs/common"
 import * as request from "supertest"
+import * as dotenv from "dotenv"
+dotenv.config({ path: "./test.env" })
 import { AppModule } from "../src/api/app.module"
+import * as dynamoose from "dynamoose"
+import { ddb } from "../src/common/database/ddb"
+
+dynamoose.aws.ddb.set(ddb)
 
 describe("AppController (e2e)", () => {
   let app: INestApplication
+  const adminToken = "admin4"
+  const studentToken = "student2"
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile()
 
     app = moduleFixture.createNestApplication()
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    )
     await app.init()
   })
 
-  it("/ (GET)", () => {
-    return request(app.getHttpServer())
-      .get("/")
-      .expect(200)
-      .expect("Hello World!")
+  // /admin/school
+  describe("POST /admin/school", () => {
+    const baseRouter = "/admin/school"
+
+    it("/ (POST) 403", () => {
+      return request(app.getHttpServer())
+        .post(baseRouter)
+        .set("Authorization", `Bearer ${adminToken}`)
+        .send({
+          location: "서울",
+          name: "서울대학교",
+        })
+        .expect(403)
+    })
   })
 })
