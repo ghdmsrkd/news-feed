aecb2b61d99525c32d96cd951295919fc53908b0
hama <hama@vonvon.me>
2022-06-19 17:00:29 +0900

    feat : /admin/school/news test코드 추가

--- test/integration/admin/school/admin-school.e2e-spec.ts
+++ test/integration/admin/school/admin-school.e2e-spec.ts
@@ -9,19 +9,46 @@ describe("AppController (e2e)", () => {
     testRequest = await getTestRequest()
   })
 
-  // /admin/school
   describe("POST /admin/school", () => {
     const baseRouter = "/admin/school"
+
+    const testBody = {
+      location: "테스트",
+      name: "테스트대학교",
+    }
+
+    it("/ (POST) 202", () => {
+      return testRequest
+        .post(baseRouter)
+        .set("Authorization", `Bearer ${adminToken}`)
+        .send(testBody)
+        .expect(201)
+    })
 
     it("/ (POST) 403", () => {
       return testRequest
         .post(baseRouter)
         .set("Authorization", `Bearer ${adminToken}`)
-        .send({
-          location: "서울",
-          name: "서울대학교",
-        })
-        .expect({ status: 403, message: "해당 항목이 이미 존제 합니다." })
+        .send(testBody)
+        .expect(403)
+    })
+  })
+
+  describe("POST /admin/school/news", () => {
+    const baseRouter = "/admin/school/news"
+
+    const testBody = {
+      school_code: "테스트-테스트대학교",
+      title: "테스트대학교title",
+      context: "테스트대학교context",
+    }
+
+    it("/ (POST) 202", () => {
+      return testRequest
+        .post(baseRouter)
+        .set("Authorization", `Bearer ${adminToken}`)
+        .send(testBody)
+        .expect(201)
     })
   })
 })
