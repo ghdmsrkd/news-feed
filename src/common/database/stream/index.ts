af283c9ec78bb781a068723e738dc039984446b7
hama <hama@vonvon.me>
2022-06-19 14:50:51 +0900

    feat : StreamEventName 추가

---
+++ src/common/database/stream/index.ts
@@ -0,0 +1,5 @@
+export const StreamEventName = {
+  INSERT: "INSERT",
+  MODIFY: "MODIFY",
+  REMOVE: "REMOVE",
+}
--- src/handler/stream-handler.ts
+++ src/handler/stream-handler.ts
@@ -4,6 +4,7 @@ import { TaskService } from "../stream/task/task.service"
 import { insertMockEvent, modifyMockEvent } from "../stream/mock-event"
 import * as AWS from "aws-sdk"
 import { ISchoolNews } from "../common/database/ddb/school-news/school-news.model"
+import { StreamEventName } from "../common/database/stream"
 
 exports.streamHandler = async (event) => {
   if (process.env.NODE_ENV === "dev") {
@@ -20,11 +21,11 @@ exports.streamHandler = async (event) => {
       record.dynamodb.NewImage,
     ) as ISchoolNews
 
-    if (record.eventName === "INSERT") {
+    if (record.eventName === StreamEventName.INSERT) {
       // 새로운 학교 뉴스가 생성 되었을 때
       const insertResult = await tasksService.insertFeedEachStudents(schoolNews)
       console.log(insertResult)
-    } else if (record.eventName === "MODIFY") {
+    } else if (record.eventName === StreamEventName.MODIFY) {
       // 기존 학교 뉴스가 수정 되어 을 때
       const updateResult = await tasksService.updateFeedEachStudents(schoolNews)
       console.log(updateResult)
